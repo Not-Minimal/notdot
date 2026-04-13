e3ec875d22e8b3b4a1e6fee711dac9c261ed5c8e
Not-Minimal <sam.pedreros14@gmail.com>
2026-04-12 21:14:23 -0400

    se agrega toggleterm

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,8 +44,44 @@ vim.pack.add({
 	{ src = "https://github.com/akinsho/toggleterm.nvim"},
 })
 require("toggleterm").setup({
-	direction = "float",}
-)
+	direction = "float",
+	open_mapping = [[<C-\>]],
+	insert_mappings = true,
+	terminal_mappings = true,
+	persist_size = true,
+	shade_terminals = true,
+	close_on_exit = false,
+	float_opts = {
+		border = "rounded",
+		winblend = 0,
+	},
+	highlights = {
+		Normal = { guibg = "#0d0f15" },
+		NormalFloat = { link = "Normal" },
+		FloatBorder = { guifg = "#3d425a", guibg = "#0d0f15" },
+	},
+})
+
+-- Custom toggle terminal (mantiene el proceso activo)
+local Terminal = require("toggleterm.terminal").Terminal
+local default_terminal = Terminal:new({
+	hidden = false,
+})
+
+function _toggle_terminal()
+	default_terminal:toggle()
+end
+
+-- Cierra y mata la terminal completamente
+function _close_terminal_completely()
+	if default_terminal then
+		default_terminal:close()
+		default_terminal:shutdown()
+		default_terminal = Terminal:new({
+			hidden = false,
+		})
+	end
+end
 -- Colorscheme
 vim.cmd.colorscheme "tundra"
 
@@ -405,6 +441,22 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<Space><Space>', ':Telescope find_files<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<Space>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Space>t', '<Cmd>lua _toggle_terminal()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Space>T', '<Cmd>lua _close_terminal_completely()<CR>', { noremap = true, silent = true })
+--:
+
+-- Terminal keymaps
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], opts)
+	vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+	vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
 --:
 
 -- LSP keymaps on attach
